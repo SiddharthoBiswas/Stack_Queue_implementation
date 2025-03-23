715918003c9c405a28d3ac1f96b41357d35b72f3
Siddhartho Biswas <siddharthobiswas2018@gmail.com>
2025-03-22 22:23:18 -1200

    Stack_using_class.py

--- stack_1.py
+++ stack_1.py
@@ -1,29 +1,44 @@
-# Python program to
-# demonstrate stack implementation
-# using list
-
-stack = []
-
-# append() function to push
-# element in the stack
-stack.append('a')
-stack.append('b')
-stack.append('c')
-
-print('Initial stack')
-print(stack)
-
-# pop() function to pop
-# element from stack in
-# LIFO order
-print('\nElements popped from stack:')
-print(stack.pop())
-print(stack.pop())
-print(stack.pop())
-
-print('\nStack after elements are popped:')
-print(stack)
-
-# uncommenting print(stack.pop())
-# will cause an IndexError
-# as the stack is now empty
+# -*- coding: utf-8 -*-
+class stack:
+    def __init__(self,Max):
+        self.top = -1
+        self.size = Max
+        self.arr = [None]*Max
+        
+    def push(self,item):
+        if(self.top == self.size-1):
+            print("\n Stack is full !")
+            
+        else:
+            self.top = self.top + 1
+            self.arr[self.top] = item
+            
+    def pop(self):
+        if (self.top == -1):
+            print("The Stack is empty")
+        else:
+            x = self.arr[self.top]
+            self.top = self.top - 1
+            return x
+        
+    def display(self):
+        if(self.top == -1):
+            print("\n Stack is empty")
+        else:
+            print("\n Elements in the stack are:")
+            for i in range(0,self.top + 1):
+                print(self.arr[i],end = " ")
+                
+if __name__ == "__main__":
+    obj =stack(5)
+    obj.display()
+    obj.push(90)
+    obj.push(10)
+    obj.push(20)
+    obj.push(30)
+    obj.display()
+    print("\n The  1st deleted element is :",obj.pop())
+    print("\n The  2nd deleted element is :",obj.pop())
+    obj.display()
+    obj.push(23)
+    obj.display()
